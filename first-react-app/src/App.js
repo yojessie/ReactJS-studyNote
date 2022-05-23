7e4d073b4a6fa730a6778fb949181219bbf1024a
yojessie <sprout.jessie@gmail.com>
2022-05-23 22:30:14 +0900

    🚧  Study cleanUp function

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,33 +1,22 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [value, setValue] = useState(0);
-  const onClick = () => setValue((value) => value + 1);
-
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => setKeyword(event.target.value);
-
+function Title() {
   useEffect(() => {
-    console.log("Like call the API...");
-    // []안에 넣어주는 것은 dependencyList이다. the things ReactJS should watch.
-    // state가 변해서 rerender 되더라도 useEffect 안에 있는 코드는 모든 요소가 렌더될때마다 실행되지 않고 dependency에 따른다.
-    // dependency가 없으므로 한번만 실행된다.
-    // dependency는 array이므로 여러개 설정이 가능하다.
+    console.log("It will run when the title is rendered");
+    return () => console.log("The title is destroyed");
+    // cleanUp function : 컴포넌트가 destroy될때 실행시키고 싶은 코드가 있다면, return 함수 안에 넣어주면 된다.
   }, []);
-  useEffect(() => {
-    console.log("It will run everytime");
-  }, [value]);
-  useEffect(() => {
-    if ((keyword !== "") & (keyword.length > 5)) {
-      console.log("search for", keyword);
-    }
-  }, [keyword]);
+  return <h1>This is title</h1>;
+}
 
+function App() {
+  const [showTitle, setShowTitle] = useState(false);
+
+  const onClick = () => setShowTitle((boolean) => !boolean);
   return (
     <div>
-      <input value={keyword} placeholder="search here" onChange={onChange} />
-      <h1>{value}</h1>
-      <button onClick={onClick}>Click me</button>
+      <button onClick={onClick}> {showTitle ? "hide" : "show"}</button>
+      {showTitle ? <Title /> : null}
     </div>
   );
 }
