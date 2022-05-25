37bfc0d1d429361583a5f5f2afc17fa6d79140d8
yojessie <sprout.jessie@gmail.com>
2022-05-25 22:39:52 +0900

    ✨ Implement a coin tracker

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,39 +1,34 @@
-import { useState } from "react";
-import { unstable_renderSubtreeIntoContainer } from "react-dom";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  // useState값이 그냥 비어있으면 undefined 되기때문에 빈 arr를 임의로 넣어준다.
+  // 화면에 코인 총 갯수를 표시하려고 length를 사용하는데, undefined로 두면 에러가 뜬다.
 
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodoList((currentArr) => [todo, ...currentArr]);
-    setTodo("");
-  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json()) // fetch로 받아온 데이터를 받아서 json으로 변환
+      .then((json) => {
+        setCoins(json); // 변환한 데이터를 coins에 넣어준다
+        setLoading(false);
+      });
+  }, []); // 처음 화면 렌더 시 한번만 실행시키기
 
   return (
-    <div onSubmit={onSubmit}>
-      <h1>My todo list ({todoList.length})</h1>
-      <form>
-        <input
-          value={todo}
-          onChange={onChange}
-          type="text"
-          placeholder="write your to-do list"
-        />
-        <button>save the list</button>
-      </form>
-      <hr />
+    <div>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
       <ul>
-        {todoList.map((item, index) => (
-          <li key={index}>{item}</li>
+        {coins.map((coin) => (
+          <li>
+            {coin.name}({coin.symbol}) : $ {Math.round(coin.quotes.USD.price)}
+          </li>
         ))}
       </ul>
     </div>
+    // map 함수를 사용했을때 받아온 인자는, arr의 각 item을 의미한다.
+    // 받아온 데이터에 id값이 있기 때문에, key로 사용할 index를 받아오지 않아도 됨.
   );
 }
 
