59f9b9834d705643f61656132014c8c99f6956b6
yojessie <sprout.jessie@gmail.com>
2022-05-31 10:05:38 +0900

    ♻️  Refactoring the components by using a Router

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,53 +1,19 @@
-import { useState, useEffect } from "react";
-import Movie from "./components/Movie";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // }, []);
-  // 위 코드는 async, await으로 아래와같이 바꿔볼 수 있다.
-
-  const getMovies = async () => {
-    const response = await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
-    );
-    const json = await response.json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies(); // useEffect의 ()에 왜 바로 함수명을 안적고, 또 함수프레임을 만들어서 그 안에 넣어야하지??
-  }, []);
-
   return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <Movie
-              key={movie.id}
-              coverImg={movie.medium_cover_image}
-              title={movie.title}
-              summary={movie.summary}
-              genres={movie.genres}
-            />
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- first-react-app/src/components/Movie.js
+++ first-react-app/src/components/Movie.js
@@ -1,10 +1,15 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+// a 태그를 통해 제목에 링크를 걸어줄 수도 있지만, 그러면 페이지 전체가 리로드 되어버린다.
+// router의 link를 활용해서 처리해주면 전체 페이지 리로드가 되지 않고 화면이 넘어간다.
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((genre) => (
